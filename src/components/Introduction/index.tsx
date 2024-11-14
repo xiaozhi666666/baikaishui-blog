d153ee7f4b92fc6c3fe3927de4ac12125e6363e3
小小智 <464691519@qq.com>
2024-11-14 18:46:49 +0800

    refactor(homepage): 移除主页特性组件和样式
    - 删除了 HomepageFeatures 组件及其相关样式
    - 移除了 index.tsx 中的 HomePageHeader 函数和相关样式
    - 在主页中添加了 Introduction 组件

---
+++ src/components/Introduction/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styles from "./index.module.less";
+import HeroSvg from "@site/static/svg/hero.svg";
+
+const Introduction = () => {
+  return (
+    <div className={styles.introduction}>
+      <div className={styles.introductionCon}>
+        <div className={styles.introductionConGreet}>
+          你好！我是{" "}
+          <span
+            className={styles.name}
+            onMouseMove={(e) => {
+              const bounding = e.currentTarget.getBoundingClientRect();
+              e.currentTarget.style.backgroundPositionX = `${
+                e.clientX - bounding.x - 150
+              }px`;
+              e.currentTarget.style.backgroundPositionY = `${
+                e.clientY - bounding.y - 150
+              }px`;
+            }}
+          >
+            白开水
+          </span>
+        </div>
+        <div className={styles.introductionConDesc}>
+          在这里我会分享一些在我日常开发中遇到的问题，或者在我日常学习中记录的一些笔记，希望我的博客会对你有所帮助!
+        </div>
+      </div>
+      <div className={styles.hero}>
+        <HeroSvg />
+        <div className={styles.heroBg} />
+      </div>
+    </div>
+  );
+};
+
+export default Introduction;
